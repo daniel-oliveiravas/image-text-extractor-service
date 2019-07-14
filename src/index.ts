603227f9b8527dc9e18891b63d2c54f2bcc808ce
Daniel Vasconcelos <daniel.oliveiravas@gmail.com>
2019-07-14 18:28:45 -0300

    Add endpoint to fetch text from image

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
+import { getText } from './service/rekognition';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+const SERVER_PORT = process.env.PORT || 8080;
+
 (async () => {
-    console.log('Initializing...');
+    const app = express();
+    app.use(bodyParser.json());
+
+    app.post('/', bodyParser.raw({ type: 'image/*' }), (req, res) => {
+        const imageBuffer = req.body;
+
+        if (!imageBuffer) {
+            return res.status(400).send('Send an image as binary body');
+        }
+
+        getText(imageBuffer)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(error => {
+                res.status(500).send(error);
+            });
+    });
+
+    console.log(`Listening port ${SERVER_PORT}`);
+    app.listen(SERVER_PORT);
 })();
